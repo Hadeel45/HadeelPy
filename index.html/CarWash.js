977b18435bc6c2d718c8b3d0a8c9ed713b130f48
Hadeel45 <93021837+Hadeel45@users.noreply.github.com>
2023-08-11 21:59:04 +0300

    Add files via upload

---
+++ index.html/CarWash.js
@@ -0,0 +1,65 @@
+function displayBrief() {
+    var briefElement = document.getElementById('homeSelection');
+    
+    // Check if the brief is already visible
+    if (briefElement.style.display === 'block') {
+      briefElement.style.display = 'none'; // Hide the brief
+    } else {
+      briefElement.style.display = 'block'; // Show the brief
+    }
+  }
+
+  function displayBrief1() {
+    var briefElement1 = document.getElementById('service');
+  
+    // Check if the brief is already visible
+    if (briefElement1.style.display === 'block') {
+      briefElement1.style.display = 'none'; // Hide the brief
+    } else {
+      briefElement1.style.display = 'block'; // Show the brief
+    }
+  }
+
+  function normalWash() {
+    var briefNormal = document.getElementById('normalBreif');
+  
+    // Check if the brief is already visible
+    if (briefNormal.style.display === 'block') {
+      briefNormal.style.display = 'none'; // Hide the brief
+    } else {
+      briefNormal.style.display = 'block'; // Show the brief
+    }
+  }
+
+  function vipWash() {
+    var briefVIP = document.getElementById('vipBreif');
+  
+    // Check if the brief is already visible
+    if (briefVIP.style.display === 'block') {
+      briefVIP.style.display = 'none'; // Hide the brief
+    } else {
+      briefVIP.style.display = 'block'; // Show the brief
+    }
+  }
+
+  function allWash() {
+    var briefALL = document.getElementById('allBreif');
+  
+    // Check if the brief is already visible
+    if (briefALL.style.display === 'block') {
+      briefALL.style.display = 'none'; // Hide the brief
+    } else {
+      briefALL.style.display = 'block'; // Show the brief
+    }
+  }
+
+  function displayBrief2() {
+    var briefNotes= document.getElementById('notesAnd');
+  
+    // Check if the brief is already visible
+    if (briefNotes.style.display === 'block') {
+      briefNotes.style.display = 'none'; // Hide the brief
+    } else {
+      briefNotes.style.display = 'block'; // Show the brief
+    }
+  }
